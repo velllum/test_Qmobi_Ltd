6926265a2622b1a714fb12cd12d0926d7d6528d1
velllum <velllum@ya.ru>
2021-02-04 16:53:28 +0300

    create server

--- server.py
+++ server.py
@@ -1,29 +1,27 @@
-# # server.py
-# import http.server # Our http server handler for http requests
-# import socketserver # Establish the TCP Socket connections
-#
-# PORT = 9000
-#
-# class GetHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
-#     def do_GET(self):
-#         self.path = '/api/index.html'
-#         print(self.path)
-#         return http.server.SimpleHTTPRequestHandler.do_GET(self)
-#
-#     # def do_POST(self):
-#     #     self.path = 'index.html'
-#     #     return http.server.SimpleHTTPRequestHandler.do_POST(self)
-# #
-# class PostHttpRequestHandler(http.server.CGIHTTPRequestHandler):
-#     def do_POST(self):
-#         self.path = 'index.html'
-#         return http.server.CGIHTTPRequestHandler.do_POST(self)
-#
-#
-#
-# Handler = GetHttpRequestHandler
-#
-#
-# with socketserver.TCPServer(("", PORT), Handler) as httpd:
-#     print("Http Server Serving at port", PORT)
-#     httpd.serve_forever()
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from io import BytesIO
+
+
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+
+
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b'Hello, world!')
+
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        body = self.rfile.read(content_length)
+        self.send_response(200)
+        self.end_headers()
+        response = BytesIO()
+        response.write(b'This is POST request. ')
+        response.write(b'Received: ')
+        response.write(body)
+        self.wfile.write(response.getvalue())
+
+
+httpd = HTTPServer(('localhost', 8000), SimpleHTTPRequestHandler)
+httpd.serve_forever()
