fc982b59ed549f4e86634eb773d6841d45f5e7eb
velllum <velllum@yandex.ru>
2021-02-06 20:31:46 +0300

    Creating a server getting started with the parser

--- server.py
+++ server.py
@@ -1,116 +1,132 @@
-# import codecs
-# import json
-# import socketserver
-# from http.server import HTTPServer, BaseHTTPRequestHandler
-#
-#
-# class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
-#
-#
-#     dic = {
-#         "валюта": "654654654",
-#         "запрошенное значение": 50,
-#         "результирующее значение": 250,
-#     }
-#
-#     def _set_headers(self):
-#         self.send_response(200)
-#         self.send_header('Content-type', 'application/json')
-#         self.end_headers()
-#
-#
-#     def do_GET(self):
-#         self._set_headers()
-#
-#         text = json.dumps(self.dic, sort_keys=True, indent=4)
-#
-#         text = codecs.decode(text, 'unicode_escape')
-#
-#         print(self.dic)
-#
-#         print(self.path)
-#
-#         self.wfile.write(bytes(text, "utf8"))
-#
-#
-#
-#
-#
-#
-#     def do_POST(self):
-#         self._set_headers()
-#
-#         self.cgi_directories = ['/', '/cgi-bin', '/htbin', '/hello.py']
-#
-#         content_length = int(self.headers['Content-Length'])
-#
-#         text = self.rfile.read(content_length)
-#
-#         print(text.decode("utf8"))
-#         text_ = text.decode("utf8")
-#         print(dict(json.loads(text_)))
-#
-#         post_dic = dict(json.loads(text_))
-#
-#         self.dic["валюта"] = post_dic["валюта"]
-#
-#         self.wfile.write(text)
-#
-#         print(self.dic)
-#
-#
-#
-#
-# httpd = HTTPServer(('localhost', 8080), SimpleHTTPRequestHandler)
-# print("сервер работает")
-# httpd.serve_forever()
+import codecs
+import json
+
+from http import HTTPStatus
+from http.server import HTTPServer, BaseHTTPRequestHandler
 
 
-#!/usr/bin/env python3
-"""
-Very simple HTTP server in python for logging requests
-Usage::
-    ./server.py [<port>]
-"""
-from http.server import BaseHTTPRequestHandler, HTTPServer
-import logging
 
-class S(BaseHTTPRequestHandler):
-    def _set_response(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+
+    dic = {}
+    kod = None
+    message = None
+    description = None
+    json_data = None
+    post_data = None
+    status = HTTPStatus
+
+
+
+    """Заполнить поля статуса сервера"""
+    def _fields_status(self):
+        return {
+            "status": {
+                "description": self.status.description,
+                "enum_name": self.status.phrase,
+                "code": self.status.value,
+            }
+        }
+
+    """Заполнить поля данные, по умолчанию равные нолю"""
+    def _fields_items(self):
+        return {
+            "Результат": 0,
+            "Сумма": "Укажите сумму конвертации",
+            "Валюта": "Укажите USD или RUB",
+        }
+
+
+    """Создаем словарь полей с проверкой на ошибку"""
+    def _make_dict(self):
+        if self.status != 200:
+            self.dic["error"] = self._fields_status()
+        else:
+            self.dic = self._fields_status()
+            self.dic["status"]["items"] = self._fields_items()
+
+
+    """Проверка ошибки"""
+    def _is_error(self):
+        if self.command == "POST" and self.path != "/":
+            print(self.command, self.path)
+            self.status = HTTPStatus.NOT_IMPLEMENTED
+
+        elif self.path != "/":
+            self.status = HTTPStatus.NOT_FOUND
+            print("GET",self.command)
+
+        else:
+            self.status = HTTPStatus.OK
+
+        self._make_dict()
+
+
+    """Добавляем запрос"""
+    def _set_headers(self):
+        self._is_error()
+        self.send_response(self.status.value)
+        self.send_header('Content-type', 'application/json')
         self.end_headers()
 
+
+    """Конвертируем словарь в json массив"""
+    def _get_json(self):
+        json_data = json.dumps(self.dic, sort_keys=True, indent=4)
+        json_data = codecs.decode(json_data, 'unicode_escape')
+        self.json_data = bytes(json_data, 'utf8')
+
+
+    """Получаем данные с post запроса"""
+    def _get_post_data(self):
+        content_length = int(self.headers['Content-Length'])
+        self.post_data = self.rfile.read(content_length)
+
+
+    """Конвертируем json в словарь"""
+    def _get_dict(self):
+        post_data = self.post_data.decode("utf8")
+        self.dic_data= dict(json.loads(post_data))
+
+
+    """get запроса"""
     def do_GET(self):
-        logging.info("GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
-        self._set_response()
-        self.wfile.write("GET request for {}".format(self.path).encode('utf-8'))
+        self._set_headers()
+        self._get_json()
+        self.wfile.write(self.json_data)
 
+
+    """post запроса"""
     def do_POST(self):
-        content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
-        post_data = self.rfile.read(content_length) # <--- Gets the data itself
-        logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
-                str(self.path), str(self.headers), post_data.decode('utf-8'))
+        self._set_headers()
+        self._get_post_data()
+        self._get_dict()
 
-        self._set_response()
-        self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
+        #==============================
+        # Обрабатываем полученные данные
+        print(self.dic)
+        print("dic_data", self.dic_data)
+        #================================
 
-def run(server_class=HTTPServer, handler_class=S, port=8080):
-    logging.basicConfig(level=logging.INFO)
-    server_address = ('', port)
+        # создать парсер для обработки
+
+        self._get_json()
+        self.wfile.write(self.json_data)
+
+
+
+def run(server_class=HTTPServer, handler_class=BaseHTTPRequestHandler):
+    server_address = ('', 8000)
     httpd = server_class(server_address, handler_class)
-    logging.info('Starting httpd...\n')
+
     try:
+        print(f"(START) Serving HTTP on {httpd.server_address[0]} port {httpd.server_port} (http://{httpd.server_address[0]}:{httpd.server_port}/)")
         httpd.serve_forever()
+
     except KeyboardInterrupt:
-        pass
-    httpd.server_close()
-    logging.info('Stopping httpd...\n')
+        print(f"(STOP) Serving HTTP on {httpd.server_address[0]} port {httpd.server_port} (http://{httpd.server_address[0]}:{httpd.server_port}/)")
+        httpd.server_close()
+
 
 if __name__ == '__main__':
-    from sys import argv
-
-    if len(argv) == 2:
-        run(port=int(argv[1]))
-    else:
-        run()
+    run(handler_class=SimpleHTTPRequestHandler)
